78e04fe530dd953e203a5732a6929ddc39382455
HOJUN LEE <75537234+hopar96@users.noreply.github.com>
2024-08-06 02:06:24 +0900

    버그 수정

--- components/blog/relateBlogs.tsx
+++ components/blog/relateBlogs.tsx
@@ -68,7 +68,7 @@ export default async function RelateBlogs({ blogId, blogCateId }: { blogId: numb
 }
 
 type ISelectBlog  = Blog & {
-  mainAtFile?: AtFile
+  mainAtFile: AtFile | null
 }
 
 function BlogCard({ blog }: { blog: ISelectBlog }) {
